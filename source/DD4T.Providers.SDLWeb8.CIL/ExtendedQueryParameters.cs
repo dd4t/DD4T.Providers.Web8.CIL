90f2add38135f511b63303caa660c2a5caa8c122
Guus Beltman <guusbeltman@gmail.com>
2016-11-11 00:27:49 +0100

    Update ExtendedQueryParameters.cs

--- source/DD4T.Providers.SDLWeb8.CIL/ExtendedQueryParameters.cs
+++ source/DD4T.Providers.SDLWeb8.CIL/ExtendedQueryParameters.cs
@@ -186,7 +186,7 @@ namespace DD4T.Providers.SDLWeb8.CIL
             {
                 // Why do we need to tell Tridion what data type the field is! Its in the database already!
                 /* FIX: Does not work (null pointer exception): var sType = typeof(MetadataType).GetField(SortType.ToString()).GetValue(null) as MetadataType;*/
-+               paramSort = new CustomMetaKeyColumn(QuerySortField, SortType);
+                paramSort = new CustomMetaKeyColumn(QuerySortField, SortType);
             }
             SortDirection paramSortDirection = typeof(SortParameter).GetField(QuerySortOrder.ToString()).GetValue(null) as SortDirection;
             SortParameter sortParameter = new SortParameter(paramSort, paramSortDirection);
